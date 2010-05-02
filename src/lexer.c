c500035c71a6a8b80fd3462d6d80d1a47cbdd313
Diego Rubin <rubin.diego@gmail.com>
2010-05-02 11:42:04 -0300

    Criação da estrutura básica do shell.
    Esta estrutura é baseada na estrutura passada pelo prof. Juliano Ravasi.

---
+++ src/lexer.c
@@ -0,0 +1,6 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * lexer.c
+ * Copyright (C) Diego Rubin 2010 <rubin.diego@gmail.com>
+ * 
+ */
---
+++ src/lexer.h
@@ -0,0 +1,6 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * lexer.h
+ * Copyright (C) Diego Rubin 2010 <rubin.diego@gmail.com>
+ * 
+ */
